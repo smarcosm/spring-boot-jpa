7259495562fd2265d7832bd0ed778d7a2b452452
smarcosm <sebastiaomarcos49@gmail.com>
2022-09-12 19:30:03 -0300

    User entity and resource

---
+++ src/main/java/com/smarcosm/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.smarcosm.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smarcosm.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Isaac", "isaac@gmail.com", "9999999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
