d7942462ba2cb8ffc04e98f1bd15a79d63575b6d
smarcosm <sebastiaomarcos49@gmail.com>
2022-09-19 18:33:45 -0300

    H2 database, test profile, JPA

--- src/main/java/com/smarcosm/course/entities/Users.java
+++ src/main/java/com/smarcosm/course/entities/Users.java
@@ -3,16 +3,38 @@ package com.smarcosm.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
+public class Users implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "name", nullable = false)
 	private String name;
+
+	@Column(name = "email")
 	private String email;
+
+	@Column(name = "phone")
 	private String phone;
+
+	@Column(name = "password")
 	private String password;
-	public User(Long id, String name, String email, String phone, String password) {
+	
+	public Users () {
+		
+	}
+
+	public Users(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -20,51 +42,67 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.password = password;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
-		User other = (User) obj;
+		}
+		Users other = (Users) obj;
 		return Objects.equals(id, other.id);
 	}
 	
 	
+
 }
--- src/main/java/com/smarcosm/course/resources/UserResource.java
+++ src/main/java/com/smarcosm/course/resources/UserResource.java
@@ -5,16 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.smarcosm.course.entities.User;
+import com.smarcosm.course.entities.Users;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Isaac", "isaac@gmail.com", "9999999999", "123456");
+	public ResponseEntity<Users> findAll() {
+		Users u = new Users(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
-
 }
