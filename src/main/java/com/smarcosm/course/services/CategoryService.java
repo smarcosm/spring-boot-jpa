7e780b48b07083cae2fd29aeb48b52dfa61c03d4
smarcosm <sebastiaomarcos49@gmail.com>
2022-09-21 20:42:45 -0300

    Category

---
+++ src/main/java/com/smarcosm/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.smarcosm.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smarcosm.course.entities.Category;
+import com.smarcosm.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
