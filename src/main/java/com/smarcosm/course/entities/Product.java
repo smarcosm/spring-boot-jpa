37ca2ba1b0e6554738932e8a7c2dd319774caf94
smarcosm <sebastiaomarcos49@gmail.com>
2022-09-30 20:37:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/smarcosm/course/entities/Product.java
+++ src/main/java/com/smarcosm/course/entities/Product.java
@@ -9,14 +9,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,8 +27,9 @@ public class Product implements Serializable {
 	private Double price;
 	private String description;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_prosuct_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
