9b7041b3ec3b314b171e4f17ef289398b9fd1d2f
smarcosm <sebastiaomarcos49@gmail.com>
2022-09-30 21:42:01 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/smarcosm/course/entities/OrderItem.java
+++ src/main/java/com/smarcosm/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.smarcosm.course.entities.pk.OrderItemPK;
 
 @Entity
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 		
 	@EmbeddedId
-		private OrderItemPK id;
+		private OrderItemPK id = new OrderItemPK();
 		private Integer quantity;
 		private Double price;
 		
@@ -31,6 +32,7 @@ public class OrderItem implements Serializable {
 			this.quantity = quantity;
 			this.price = price;
 		}
+		@JsonIgnore
 		public Order getOrder() {
 			return id.getOrder();
 		}
