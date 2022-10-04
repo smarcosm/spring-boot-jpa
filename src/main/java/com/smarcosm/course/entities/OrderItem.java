cacd8c1f9f4b6683c2fbb549d04f46ec974d1112
smarcosm <sebastiaomarcos49@gmail.com>
2022-10-04 20:54:07 -0300

    Subtotal & total methods

--- src/main/java/com/smarcosm/course/entities/OrderItem.java
+++ src/main/java/com/smarcosm/course/entities/OrderItem.java
@@ -42,7 +42,6 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 	}
 
-	
 	public Product getProduct() {
 		return id.getProduct();
 	}
@@ -65,6 +64,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
